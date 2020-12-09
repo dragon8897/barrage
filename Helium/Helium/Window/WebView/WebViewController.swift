0a67efec930b9c1642349c0b3fabae3663e0751b
dragon <dragon@fire.com>
2020-12-09 18:24:25 +0800

    feat(core): 添加 websocket

--- Helium/Helium/Window/WebView/WebViewController.swift
+++ Helium/Helium/Window/WebView/WebViewController.swift
@@ -8,6 +8,7 @@
 
 import Cocoa
 import WebKit
+import Starscream
 
 class HeliumWebView: WKWebView {
     override var mouseDownCanMoveWindow: Bool {
@@ -22,6 +23,8 @@ class WebViewController: NSViewController, WKNavigationDelegate {
     override func loadView() {
         self.view = NSView()
     }
+    
+    var socket: WebSocket?
     
     // MARK: View lifecycle
     override func viewDidLoad() {
@@ -55,6 +58,52 @@ class WebViewController: NSViewController, WKNavigationDelegate {
         if let homePage = UserSetting.homePageURL {
             loadAlmostURL(homePage)
         }
+        
+        var request = URLRequest(url: URL(string: "ws://localhost:3250/nano")!)
+        request.timeoutInterval = 5
+        socket = WebSocket(request: request)
+        socket?.onEvent = { event in
+            switch event {
+            case .connected(let headers):
+                print("websocket is connected: \(headers)")
+                let s: String = """
+                {"sys":{"type":"js-websocket","version":"0.0.1","rsa":{}},"user":{}}
+                """
+                let len = s.utf8.count
+                let pkg: [UInt8] = [1 & 0xff, UInt8((len >> 16) & 0xff), UInt8((len >> 8) & 0xff), UInt8(len & 0xff)]
+                var d = Data(pkg)
+                d.append(contentsOf: s.utf8)
+                print("send", d)
+                self.socket?.write(data: d)
+            case .disconnected(let reason, let code):
+                print("websocket is disconnected: \(reason) with code: \(code)")
+            case .text(let string):
+                print("Received text: \(string)")
+            case .binary(let data):
+                let t = data[0]
+                let len = (data[1] << 16) | (data[2] << 8) | (data[3])
+                print("Received data: \(t), \(len), \(String(decoding: data[4...(data.count-1)], as: UTF8.self))")
+            case .ping(_):
+                print("websocket ping")
+                break
+            case .pong(_):
+                print("websocket pong")
+                break
+            case .viabilityChanged(let data):
+                print("viabilityChanged data: \(data)")
+                break
+            case .reconnectSuggested(_):
+                print("websocket reconnectSuggested")
+                break
+            case .cancelled:
+                print("websocket cancelled")
+                break
+            case .error(let error):
+                print("error", error ?? "")
+                break
+            }
+        }
+        
     }
     
     override func viewDidLayout() {
@@ -141,6 +190,7 @@ class WebViewController: NSViewController, WKNavigationDelegate {
         DispatchQueue.main.asyncAfter(deadline: .now()+13.0) {
             textView.removeFromSuperview()
         }
+        socket?.connect()
     }
     
     @objc func loadURLObject(_ urlObject: Notification) {
