de7cc70be5131f2a437cbda419002f30bb9027d8
dragon <dragon@fire.com>
2020-12-14 14:49:36 +0800

    feat(core): 添加 popupover 面板

--- barrage/AppDelegate.swift
+++ barrage/AppDelegate.swift
@@ -9,18 +9,44 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
 
     let controller = WindowController()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         controller.showWindow(self)
+        
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 200, height: 300)
+        popover.behavior = .transient
+        popover.contentViewController = PopupViewController()
+        self.popover = popover
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Icon")
+            button.action = #selector(togglePopover(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
 }
 
